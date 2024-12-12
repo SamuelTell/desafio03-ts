4c4358047d857eba5f5d962a353702f049922433
Samuel Telles <samuel.santos@outlook.com>
2024-12-11 22:34:21 -0300

    apply format

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,38 +8,40 @@ import { login } from "../services/login";
 import { changeLocalStorage } from "../services/storage";
 
 const Home = () => {
-    const [ email, setEmail ] = useState<string>('')
-    const { setIsLoggedIn } = useContext(AppContext)
-    const navigate = useNavigate()
+  const [email, setEmail] = useState<string>("");
+  const { setIsLoggedIn } = useContext(AppContext);
+  const navigate = useNavigate();
 
-    const validateUser = async (email: string) => {
-        const loggedIn = await login(email)
+  const validateUser = async (email: string) => {
+    const loggedIn = await login(email);
 
-        if(!loggedIn){
-            return alert('Email inválido')
-        }
-
-        setIsLoggedIn(true)
-        changeLocalStorage({ login: true })
-        navigate('/conta/1')
+    if (!loggedIn) {
+      return alert("Email inválido");
     }
-  
-    return (
-        <Box padding="25px">
-            <Card>
-                <Center>
-                    <h1>Faça o login</h1>
-                </Center>
-                <Input placeholder="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-                <Input placeholder="password" />
-                <Center>
-                    <DButton
-                        onClick={() => validateUser(email)}
-                    />
-                </Center>
-            </Card>
-        </Box>
-    );
-}
+
+    setIsLoggedIn(true);
+    changeLocalStorage({ login: true });
+    navigate("/conta/1");
+  };
+
+  return (
+    <Box padding="25px">
+      <Card>
+        <Center>
+          <h1>Faça o login</h1>
+        </Center>
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input placeholder="password" />
+        <Center>
+          <DButton onClick={() => validateUser(email)} />
+        </Center>
+      </Card>
+    </Box>
+  );
+};
 
 export default Home;
